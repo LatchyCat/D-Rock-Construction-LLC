9a3541d6aa7892ef86884c657bb840d28cce9741
oriel <orielsilvajr@gmail.com>
2024-10-15 18:20:04 -0400

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',  // Correct base setting
+  base: '/D-Rock-Construction-LLC/',  // Correct base setting
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
