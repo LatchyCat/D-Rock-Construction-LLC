1446d096a7706d8dead093bc2d6ad13921955464
oriel <orielsilvajr@gmail.com>
2024-10-15 15:55:44 -0400

    Update package.json and vite config for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/D-Rock-Construction-LLC/', // Add this line
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
